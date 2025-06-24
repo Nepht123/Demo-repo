6187b95b02c93822e8e802a6112f85546318e29a
Nepht123 <mwambanephtalie12@gmail.com>
2025-06-24 19:05:59 +0200

    Game window created
    I have created my game window and a way to shut it down

--- github_py_projects/Game_in_python/Games_Readme.md
+++ github_py_projects/Game_in_python/Games_Readme.md
@@ -1,2 +1,4 @@
 #Info file
 This is hust an info file for everything that will be goingg on in this 
+
+Damn I need to brush up on the basics of python programing. I have forgotten the structure of simple stuff like my if statements and the loops
--- github_py_projects/Game_in_python/game.py
+++ github_py_projects/Game_in_python/game.py
@@ -1,3 +1,22 @@
-import pygame
+import pygame as pg
+pg.init()
+#pygame.init() allows us to initialize and begin to use pygame features
 
-pygame.init()
+
+#Here I am creating the game screen
+screen=pg.display.set_mode((800,600))#Here I am determining the size of my screen
+'''Now this did succesfully creat the screen but the issue is that the screen will show as long as the programe is running,
+   but the programe only takes a few seconds to finish running.
+   So I need to find a way to keep it running,
+   while still having a way to shut it down when neccesary in order to close the game
+'''
+
+#To fix this running issue we are going to use something called events
+'''An event is anything that is happening inside a game window but we will get more into it a bit later'''
+
+running =True
+while running:
+    for events in pg.event.get():#This will go through every single possible event in the pygame library 
+        if events.type == pg.QUIT:#If the event that as happened is to close the game window, this programe will stop running 
+            running=False
+
